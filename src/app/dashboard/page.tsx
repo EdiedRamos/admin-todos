bfb1bbc46e242dbcaa08a6781c2255120e7481f5
EdiedRamos <edieisa22@gmail.com>
2024-06-15 21:16:22 -0500

    feat: add dashboard component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,38 @@
+import { Card } from "@/components";
+
+export default function Dashboard() {
+  return (
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <Card
+        title="$23,988"
+        subtitle="Global Activities"
+        text="Compared to last week $13,988"
+      />
+      <Card
+        title="$23,988"
+        subtitle="Global Activities"
+        text="Compared to last week $13,988"
+      />
+      <Card
+        title="$23,988"
+        subtitle="Global Activities"
+        text="Compared to last week $13,988"
+      />
+      <Card
+        title="$23,988"
+        subtitle="Global Activities"
+        text="Compared to last week $13,988"
+      />
+      <Card
+        title="$23,988"
+        subtitle="Global Activities"
+        text="Compared to last week $13,988"
+      />
+      <Card
+        title="$23,988"
+        subtitle="Global Activities"
+        text="Compared to last week $13,988"
+      />
+    </div>
+  );
+}
