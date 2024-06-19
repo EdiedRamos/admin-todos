6ce75e1739369bc7ec2a66043342250ed75100fe
EdiedRamos <edieisa22@gmail.com>
2024-06-18 23:00:14 -0500

    feat: use the cookie value for setting the initial selectedTab value

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,15 +1,19 @@
 import { TabBar } from "@/components";
+import { cookies } from "next/headers";
 
 export const metadata = {
   title: "Cookies page",
 };
 
 export default function CookiesPage() {
+  const cookieStore = cookies();
+  const selectedTab = cookieStore.get("selectedTab")?.value ?? "1";
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
       <div className="flex flex-col">
         <p className="text-3xl">Tabs</p>
-        <TabBar />
+        <TabBar currentTab={Number(selectedTab)} />
       </div>
     </div>
   );
