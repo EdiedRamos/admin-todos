2d3b06a5adc479b29eeec77cd77446c62e07250b
EdiedRamos <edieisa22@gmail.com>
2024-06-19 22:50:07 -0500

    feat: implement cookie cart

--- src/app/dashboard/products/components/ProductCard.tsx
+++ src/app/dashboard/products/components/ProductCard.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5";
 
 import Image from "next/image";
 import type { Product } from "../interfaces";
+import { addProductToCart } from "@/cart/actions/cart.actions";
 
 function generateArray(length: number): number[] {
   return Array(length)
@@ -14,6 +17,10 @@ interface Props {
 }
 
 export const ProductCard = ({ product }: Props) => {
+  const handleAddProduct = () => {
+    addProductToCart(product.id);
+  };
+
   return (
     <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-100">
       {/* Product Image */}
@@ -60,7 +67,10 @@ export const ProductCard = ({ product }: Props) => {
           </span>
 
           <div className="flex">
-            <button className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button
+              onClick={handleAddProduct}
+              className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
               <IoAddCircleOutline size={25} />
             </button>
             <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
---
+++ src/cart/actions/cart.actions.ts
@@ -0,0 +1,26 @@
+"use client";
+
+import { getCookie, hasCookie, setCookie } from "cookies-next";
+
+type CookieCart = {
+  [id: string]: number;
+};
+
+export function getCartCookie(): CookieCart {
+  if (hasCookie("cart")) {
+    const cartCookie = JSON.parse((getCookie("cart") as string) ?? "{}");
+    return cartCookie;
+  }
+  return {};
+}
+
+export function addProductToCart(id: string): void {
+  const cartCookie = getCartCookie();
+  console.log(cartCookie);
+  if (cartCookie[id]) {
+    cartCookie[id]++;
+  } else {
+    cartCookie[id] = 1;
+  }
+  setCookie("cart", JSON.stringify(cartCookie));
+}
